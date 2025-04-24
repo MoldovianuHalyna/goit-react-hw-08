d4f20647d00a2fe442754095fc345611862f1ec1
Halyna Moldovianu <Beorgeos@gmail.com>
2025-04-24 17:57:18 +0200

    3

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,11 +4,18 @@ import { useSelector } from "react-redux";
 import { selectIsLoggedIn } from "../../redux/auth/selectors";
 const Navigation = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
+  const addActive = ({ isActive }) => (isActive ? s.active : s.link);
 
   return (
     <div className={s.navBar}>
-      <NavLink to="/">Home</NavLink>
-      {isLoggedIn && <NavLink to="/contacts">Contacts</NavLink>}
+      <NavLink className={addActive} to="/">
+        Home
+      </NavLink>
+      {isLoggedIn && (
+        <NavLink className={addActive} to="/contacts">
+          Contacts
+        </NavLink>
+      )}
     </div>
   );
 };
